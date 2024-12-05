c6fcf5bbec61aa5ff5589df37e1e51430a9d25f2
julien.seemulle <julien.seemuller@etu.hesge.ch>
2024-12-05 17:05:08 +0100

    Added link to regex tester

--- day_3/2/2.js
+++ day_3/2/2.js
@@ -3,6 +3,9 @@ const inputStrings = (await readFile("day_3/2/input.txt", "utf-8")).split("\n");
 
 const dontCmd = 'don\'t()'
 const doCmd = 'do()'
+
+// c.f. regex tester to check control groups 
+// https://regex101.com/r/0TMug1/1
 const re = /(mul\((\d+),(\d+)\))|(do\(\))|don't\(\)/g;
 let total = 0;
 let shouldSkip = false;
